16d260b7a4f89d391c0bff1e4e508970068aa85a
Zak Forster <zak.forster16@gmail.com>
2019-08-19 21:46:15 +0100

    Updates

--- precache-manifest.6485b1d4f2f8d754c0ba8e55a828bd90.js
+++ precache-manifest.6485b1d4f2f8d754c0ba8e55a828bd90.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9ea474da59e0113ff636c7fcf69c097",
+    "revision": "dbbc7e91a639447e38f2f94c257d1d36",
     "url": "/zforster/index.html"
   },
   {
-    "revision": "9b8d824dd450d549f808",
-    "url": "/zforster/static/css/main.eac5e80a.chunk.css"
+    "revision": "a1980986323cee944775",
+    "url": "/zforster/static/css/main.cb94a56f.chunk.css"
   },
   {
     "revision": "f99493a3e448b56c2f86",
     "url": "/zforster/static/js/2.133f1687.chunk.js"
   },
   {
-    "revision": "9b8d824dd450d549f808",
-    "url": "/zforster/static/js/main.b80f624d.chunk.js"
+    "revision": "a1980986323cee944775",
+    "url": "/zforster/static/js/main.66795045.chunk.js"
   },
   {
     "revision": "d6d781529f670f8dcb5a",
