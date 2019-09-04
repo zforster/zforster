661fa03c394723bac884d119494e0f3f85ecf022
Zak Forster <zak.forster16@gmail.com>
2019-09-04 20:48:34 +0100

    clean scramble code

--- src/components/scrambleText.js
+++ src/components/scrambleText.js
@@ -4,9 +4,9 @@ class ScrambleText extends Component{
     constructor(){
         super();
         this.chars = ';,"\'()!<>-+/[]{}=*%#_';
-        this.phrases = ['Data Engineer', 'Computer Science Undergraduate', 'Avid Programmer'];
+        this.phrases = ['Hello & Welcome!', 'Data Engineer', 'Computer Science Undergraduate', 'Avid Programmer'];
         this.textContainer = <div className="scrambleText"/>;
-        this.update = this.update.bind(this)
+        this.update = this.update.bind(this);
     }
 
     render(){
@@ -14,61 +14,65 @@ class ScrambleText extends Component{
             this.textContainer
         );
     }
-    setText(newText) {
+
+    setText(singlePhrase) {
         const oldText = this.scrambleElement.innerText;
-        const length = Math.max(oldText.length, newText.length)
-        const promise = new Promise((resolve) => this.resolve = resolve)
-        this.queue = []
+        const length = Math.max(oldText.length, singlePhrase.length);  // get the length of either the new phrase, or the prev phrase (depending on whats bigger)
+        const promise = new Promise(resolve => this.resolve = resolve); // promise that returns true on success?
+        this.queue = [];
         for (let i = 0; i < length; i++) {
-            const from = oldText[i] || ''
-            const to = newText[i] || ''
-            const start = Math.floor(Math.random() * 40)
-            const end = start + Math.floor(Math.random() * 40)
+            const from = oldText[i] || '';
+            const to = singlePhrase[i] || '';
+            const start = Math.floor(Math.random() * 40);
+            const end = start + Math.floor(Math.random() * 40);
             this.queue.push({ from, to, start, end })
         }
-        cancelAnimationFrame(this.frameRequest)
-        this.frame = 0
-        this.update()
-        return promise
+        cancelAnimationFrame(this.frameRequest);
+        this.frame = 0;
+        this.update();
+        return promise;
     }
+
     update() {
-        let output = ''
-        let complete = 0
+        let output = '';
+        let complete = 0;
         for (let i = 0, n = this.queue.length; i < n; i++) {
-            let { from, to, start, end, char } = this.queue[i]
+            let { from, to, start, end, char } = this.queue[i];
             if (this.frame >= end) {
-                complete++
+                complete++;
                 output += to
-            } else if (this.frame >= start) {
+            }
+            else if (this.frame >= start) {
                 if (!char || Math.random() < 0.28) {
-                    char = this.randomChar()
+                    char = this.getRandomChar();
                     this.queue[i].char = char
                 }
-                output += `<span class="dud">${char}</span>`
-            } else {
+                output += `<span>${char}</span>`
+            }
+            else {
                 output += from
             }
         }
-        this.scrambleElement.innerHTML = output
+        this.scrambleElement.innerHTML = output;
         if (complete === this.queue.length) {
             this.resolve()
-        } else {
-            this.frameRequest = requestAnimationFrame(this.update)
-            this.frame++
+        }
+        else {
+            this.frameRequest = requestAnimationFrame(this.update);
+            this.frame++;
         }
     }
-    randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)]
+
+    getRandomChar() {
+        return this.chars[Math.floor(Math.random() * this.chars.length)];
     }
 
-    componentDidMount() {
+    componentDidMount() { // this is run once the component is rendered
         this.scrambleElement = document.querySelector('.scrambleText');
         let counter = 0;
         const next = () => {
-            this.setText(this.phrases[counter]).then(() => {
-                setTimeout(next, 800)
-            })
-            counter = (counter + 1) % this.phrases.length
+            this.setText(this.phrases[counter]).then(() => { setTimeout(next, 1200) });
+            counter = (counter + 1) % this.phrases.length;
         };
         next();
     }
